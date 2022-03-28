4d8104b2aa7f75271ad79dbf4b2256956222d90c
Szekely Laszlo <nlszekely@gmail.com>
2022-03-28 19:56:35 +0300

    working animation about us

--- src/screens/Section1/Section1.js
+++ src/screens/Section1/Section1.js
@@ -5,30 +5,112 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 
+import image1 from '../../assets/about-us-animation/1.png';
+import image2 from '../../assets/about-us-animation/2.png';
+import image3 from '../../assets/about-us-animation/3.png';
+import image4 from '../../assets/about-us-animation/4.png';
+import image5 from '../../assets/about-us-animation/5.png';
+import image6 from '../../assets/about-us-animation/6.png';
+import image7 from '../../assets/about-us-animation/7.png';
+import image8 from '../../assets/about-us-animation/8.png';
+import image9 from '../../assets/about-us-animation/9.png';
+import image10 from '../../assets/about-us-animation/10.png';
+import image11 from '../../assets/about-us-animation/11.png';
+import image12 from '../../assets/about-us-animation/12.png';
+import image13 from '../../assets/about-us-animation/13.png';
+import image14 from '../../assets/about-us-animation/14.png';
+import image15 from '../../assets/about-us-animation/15.png';
+import image16 from '../../assets/about-us-animation/16.png';
+import image17 from '../../assets/about-us-animation/17.png';
+import image18 from '../../assets/about-us-animation/18.png';
+import image19 from '../../assets/about-us-animation/19.png';
+import image20 from '../../assets/about-us-animation/20.png';
+
+import './Section1.css';
+
 export default function Section1() {
     return (
         <Grid container sx={{padding: 5}}>
-            {/* <Grid item xs={12} sm={6}>
-                <img style={{maxWidth: '100%', height: 'auto'}} src={hero} />
-            </Grid>
-            <Grid item justifyContent="flex-start" xs={12} sm={6} sx={{paddingLeft: {sm: '24px'}}}>
-                <Typography component={'h1'} variant={'h3'}  >
-                    Welcome to the Silly Sloth Club
-                </Typography>
-                <Typography component={"p"} variant={'h5'} sx={{marginTop:3}}>
-                    The Silly Sloth Club is a collection of 10,000 AI-generated collectibles living on the Cardano
-                    Blockchain.
-                </Typography>
-                <Typography component={"p"}  variant={'h5'} sx={{marginTop:2}}>
-                    All 10,000 Silly Sloths are programmatically randomly generated from more than 115 attributes witch
-                    blends together and gives a unic personality to each Sloth.
-                </Typography>
-                <Grid container sx={{marginTop:3}} justifyContent='center'>
-                    <Button sx={{height:35}} variant='contained' color='secondary'>
-                        <Typography>BUY ON CNFT</Typography>
-                    </Button>
-                </Grid>
-            </Grid> */}
+            <div style={{borderTopLeftRadius: 35, borderBottomLeftRadius: 35}} className='carousel-container'>
+                <div className='gradient-top'></div>
+
+                <div
+                    className='mover-1'
+                    style={{
+                        maxWidth: '300px',
+                    }}
+                >
+                    <img className='img' src={image1} />
+                    <img className='img' src={image2} />
+                    {/* <img className='img' src={image3} />
+                    <img className='img' src={image4} />
+                    <img className='img' src={image5} />
+                    <img className='img' src={image6} />
+                    <img className='img' src={image7} />
+                    <img className='img' src={image8} />
+                    <img className='img' src={image9} />
+                    <img className='img' src={image10} /> */}
+                </div>
+                <div
+                    className='mover-2'
+                    style={{
+                        maxWidth: '300px',
+                    }}
+                >
+                    <img className='img' src={image1} />
+                    <img className='img' src={image2} />
+                    {/* <img className='img' src={image3} />
+                    <img className='img' src={image4} />
+                    <img className='img' src={image5} />
+                    <img className='img' src={image6} />
+                    <img className='img' src={image7} />
+                    <img className='img' src={image8} />
+                    <img className='img' src={image9} />
+                    <img className='img' src={image10} /> */}
+                </div>
+                <div className='gradient-bottom'></div>
+
+            </div>
+{/* 
+            <div style={{borderTopRightRadius:35, borderBottomRightRadius:35}} className='carousel-container'>
+                <div className='gradient-top'></div>
+
+                <div
+                    className='mover-1-reverse'
+                    style={{
+                        maxWidth: '300px',
+                    }}
+                >
+                    <img className='img' src={image11} />
+                    <img className='img' src={image12} />
+                    <img className='img' src={image13} />
+                    <img className='img' src={image14} />
+                    <img className='img' src={image15} />
+                    <img className='img' src={image16} />
+                    <img className='img' src={image17} />
+                    <img className='img' src={image18} />
+                    <img className='img' src={image19} />
+                    <img className='img' src={image20} />
+                </div>
+                <div
+                    className='mover-2-reverse'
+                    style={{
+                        maxWidth: '300px',
+                    }}
+                >
+                    <img className='img' src={image11} />
+                    <img className='img' src={image12} />
+                    <img className='img' src={image13} />
+                    <img className='img' src={image14} />
+                    <img className='img' src={image15} />
+                    <img className='img' src={image16} />
+                    <img className='img' src={image17} />
+                    <img className='img' src={image18} />
+                    <img className='img' src={image19} />
+                    <img className='img' src={image20} />
+                </div>
+                <div className='gradient-bottom'></div>
+            </div> */}
         </Grid>
     );
 }
